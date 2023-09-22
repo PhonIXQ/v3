2a1d43522eb6031f956de734143dc3b78b0a08b7
PhonIXQ <logophonixo@hotmail.com>
2023-09-22 20:01:23 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio v3`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
